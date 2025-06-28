2373548799f98c78731e8c168c65ffc541a02ba3
Anton <antgal2003@gmail.com>
2025-06-28 09:13:10 +0300

    текст комментария

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -4,30 +4,30 @@ from .models import Post, Group, Comment, Follow
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'pub_date', 'author', 'group')
-    list_filter = ('pub_date', 'group')
-    search_fields = ('text',)
-    empty_value_display = '-пусто-'
+    list_display = ("id", "text", "pub_date", "author", "group")
+    list_filter = ("pub_date", "group")
+    search_fields = ("text",)
+    empty_value_display = "-пусто-"
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'description')
-    prepopulated_fields = {'slug': ('title',)}
-    search_fields = ('title',)
-    empty_value_display = '-пусто-'
+    list_display = ("id", "title", "slug", "description")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'post', 'text', 'created')
-    list_filter = ('created',)
-    search_fields = ('text',)
-    empty_value_display = '-пусто-'
+    list_display = ("id", "author", "post", "text", "created")
+    list_filter = ("created",)
+    search_fields = ("text",)
+    empty_value_display = "-пусто-"
 
 
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'following')
-    search_fields = ('user__username', 'following__username')
-    empty_value_display = '-пусто-'
+    list_display = ("id", "user", "following")
+    search_fields = ("user__username", "following__username")
+    empty_value_display = "-пусто-"
